0e584efa1f30f91b2eb098598454fca709694346
Askkyy <ask.ask.air@gmail.com>
2023-05-09 14:39:54 -1000

    add docker

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import selenium.webdriver as se
-import chromedriver_binary
 from selenium.webdriver.support.ui import WebDriverWait  # for implicit and explict waits
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
-import csv,os
+import csv,os,time
 
+def set_chrome_options() -> Options:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
 
-def collect_info(shortURL):
+if __name__ == "__main__":
+    driver = webdriver.Chrome(options=set_chrome_options())
+    # Do stuff with your driver
+    shortURL = input("ShortURL >>>" )
     url = f"https://www.instagram.com/p/{shortURL}"
-    option = se.ChromeOptions() #add option
-    option.add_argument('headless') # Don't show browser
-    option.add_experimental_option("detach", False) #close window after finishing
-    browser = se.Chrome('path/to/chromedriver',options=option) # open browser
-    browser.get(url)
-    parse_html= BeautifulSoup(browser.page_source,"html.parser") # Scrape url
+    driver.get(url)
+    time.sleep(4)
+    parse_html= BeautifulSoup(driver.page_source,"html.parser") # Scrape url
     date = parse_html.find(class_="_aaqe")
     like= parse_html.find(class_="x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj")
     content = parse_html.find(class_ ="_aacl _aaco _aacu _aacx _aad7 _aade")
@@ -35,5 +47,4 @@ def collect_info(shortURL):
     with open(csvPath, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([date,like,content,url])
-
-collect_info(input("ShortURL >>>" ))
+    driver.close()
